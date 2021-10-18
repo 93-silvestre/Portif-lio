442005675b6098a3f8678bf6504e64c6c5b481f6
93-silvestre <nathyribsilvestre@gmail.com>
2021-10-17 22:43:31 -0300

    Animações de Scroll

--- static/script.js
+++ static/script.js
@@ -23,4 +23,29 @@ navItem.forEach((item) => {
   });
 });
 
+/* Animação de todos os itens que tenha atributo data-anime*/
+
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScrool = () => {
+  const windowTop = window.pageYOffset + window.innerHeight * 0.85;
+  
+
+  item.forEach(element =>{
+    if (windowTop > element.offsetTop) {
+      element.classList.add("animate");
+    } else {
+      element.classList.remove("animate");
+    }
+  })
+
+}
+
+animeScrool()
+
+window.addEventListener("scroll", ()=>{
+  animeScrool();
+})
+
+
 
